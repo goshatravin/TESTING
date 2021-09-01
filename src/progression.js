7cc531e0756c9dbbe241497992ad86240ef5b40a
George <goshatravin@gmail.com>
2021-09-01 15:47:03 +0300

    fix

--- src/progression.js
+++ src/progression.js
@@ -2,7 +2,8 @@ import random from './randomNumber.js';
 
 const progression = () => {
   const number = random();
-  const diff = Math.floor(Math.random() * 10);
+  const diff = Math.round(Math.random() * 10 + 1);
+  console.log(diff, 'diff')
   const arr = [];
   let answer = '';
   for (let i = 1; i <= 10; i += 1) {
